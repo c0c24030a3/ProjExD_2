177ec3e00788bd4e3f347a5f37608bf196f3efb7
M_Sato <c0c24030a3@edu.teu.ac.jp>
2025-06-17 17:30:20 +0900

    演習2 爆弾の巨大化

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -24,6 +24,9 @@ def check_bound(rct:pg.Rect)->tuple[bool,bool]:
     return yoko ,tate#結果を返す
 tmr=0
 def display_update():
+    """
+    ゲームオーバー画面を表示する
+    """
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     go_img=pg.image.load("fig/8.png")
     fonto=pg.font.Font(None,80)
@@ -40,8 +43,31 @@ def display_update():
     screen.blit(txt,[550,325])
     pg.display.update()
     time.sleep(5)
- 
-    print("ぬん")
+
+def bomb(tmr):
+    a_tmr=0
+    r_accs=0
+    bb_accs=[a for a in range(1,11)]
+
+    a_tmr=int(tmr/1000)
+    if a_tmr<=12:
+        r_accs=bb_accs[a_tmr]
+
+    return r_accs
+
+def yakitori(sum_mv):
+    a=yakitori.get(tuple(sum_mv)) 
+    return
+bb_imgs=[]
+bb_accs=[a for a in range (1,11)]
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0,0,0))
+        bb_imgs.append(bb_img)
+    return bb_imgs,bb_accs
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -59,10 +85,17 @@ def main():
     clock = pg.time.Clock()
     tmr = 0
     DELTA={pg.K_UP:(0,-5),pg.K_DOWN:(0,5),pg.K_LEFT:(-5,0),pg.K_RIGHT:(5,0)}
+    yakitori={(0,-5):pg.transform.rotozoom(kk_img,0,1.0),(5,-5):pg.transform.rotozoom(kk_img,45,1.0),(5,0):pg.transform.rotozoom(kk_img,90,1.0),(5,5):pg.transform.rotozoom(kk_img,125,1.0),(0,5):pg.transform.rotozoom(kk_img,180,1.0),(-5,5):pg.transform.rotozoom(kk_img,225,1.0),(-5,0):pg.transform.rotozoom(kk_img,270,1.0),(-5,-5):pg.transform.rotozoom(kk_img,315,1.0),(0,0):pg.transform.rotozoom(kk_img,0,1.0)}
+
+    bb_imgs,bb_accs=init_bb_imgs()
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        avx = vx*bb_accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
+
         if kk_rct.colliderect(bb_rct):
             display_update()
             time.sleep(2)
@@ -71,25 +104,29 @@ def main():
         screen.blit(bg_img, [0, 0]) 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+        
         for key ,mv in DELTA.items():
             if key_lst[key]:    
                 sum_mv[0]+=mv[0]
                 sum_mv[1]+=mv[1]
-        
         kk_rct.move_ip(sum_mv)
         check_bound(kk_rct)
         if check_bound(kk_rct)!=(True,True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
+        sum_mv=tuple(sum_mv)
+        kk_img = yakitori.get(tuple(sum_mv)) 
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx,vy)#爆弾の挙動
+        bb_rct.move_ip(avx,vy)#爆弾の挙動
         check_bound(bb_rct)
         yoko,tate=check_bound(bb_rct)
+        nu=bomb(tmr)
         if not yoko:
             vx*=-1
         if not tate:
             vy*=-1
-
+        
         screen.blit(bb_img, bb_rct)
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
