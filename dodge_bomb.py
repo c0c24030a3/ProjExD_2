d854f4d58f746dc16e20008765502755f09f87e0
M_Sato <c0c24030a3@edu.teu.ac.jp>
2025-06-17 14:36:39 +0900

    練習3 画面内外チェック

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -9,6 +9,20 @@ WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(rct:pg.Rect)->tuple[bool,bool]:
+    """
+    引数:こうかとんRectかばくだんRect
+    戻り値:タプル(横方向判定結果,縦方向判定結果)
+    画面内ならTrue,画面内ならFalse
+    """
+    yoko,tate=True,True #画面の中を初期値に
+    if rct.left<0 or WIDTH <rct.right:
+        yoko=False
+    if rct.top <0 or HEIGHT<rct.bottom:
+        tate=False
+    return yoko ,tate#結果を返す
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -38,8 +52,18 @@ def main():
                 sum_mv[1]+=mv[1]
         
         kk_rct.move_ip(sum_mv)
+        check_bound(kk_rct)
+        if check_bound(kk_rct)!=(True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx,vy)
+        bb_rct.move_ip(vx,vy)#爆弾の挙動
+        check_bound(bb_rct)
+        yoko,tate=check_bound(bb_rct)
+        if not yoko:
+            vx*=-1
+        if not tate:
+            vy*=-1
+
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
@@ -51,3 +75,6 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
+
+
+
